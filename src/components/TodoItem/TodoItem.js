13c5c153d50d1b02c02b46a888b56fb6420bef78
Melissa Astbury <melissaastbury@hotmail.com>
2021-03-12 12:02:04 +0000

    added styling

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './TodoItem.css';
 
+import Checkbox from '@material-ui/core/Checkbox';
+
 const TodoItem = ({ id, name, done }) => {
+  const handleCheck = () => {};
   return (
     <div className="todoItem">
-      <p>{name}</p>
+      <Checkbox
+        checked={done}
+        color="primary"
+        onChange={handleCheck}
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
+      <p className={done && 'todoItem--done'}>{name}</p>
     </div>
   );
 };
